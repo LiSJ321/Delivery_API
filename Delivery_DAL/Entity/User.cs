1874f6f8e1bc10047d8c4197a4a09440b3126bf2
LiSJ321 <126954436+LiSJ321@users.noreply.github.com>
2024-12-24 22:05:13 +0800

    feat(database): User,Basket entity created

---
+++ Delivery_DAL/Entity/User.cs
@@ -0,0 +1,40 @@
+﻿using Delivery_DAL.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delivery_DAL.Entity
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string FullName { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        public string? Address { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+    }
+}
