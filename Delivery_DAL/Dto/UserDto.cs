6a700426e6316d027934f3296a804f55a3b0aa1a
LiSJ321 <126954436+LiSJ321@users.noreply.github.com>
2024-12-24 22:10:08 +0800

    feat:Created releated dtos

---
+++ Delivery_DAL/Dto/UserDto.cs
@@ -0,0 +1,33 @@
+﻿using Delivery_DAL.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delivery_DAL.Dto
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string FullName { get; set; }
+        public DateTime? BirthDate { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        public string? Address { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+    }
+}
---
+++ Delivery_DAL/Dto/UserRegisterModel.cs
@@ -0,0 +1,35 @@
+﻿using Delivery_DAL.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delivery_DAL.Dto
+{
+    public class UserRegisterModel
+    {
+        [Required]
+        [MinLength(1)]
+        public string FullName { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string? Address { get; set; }
+        public DateTime? BirthDate { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+    }
+}
