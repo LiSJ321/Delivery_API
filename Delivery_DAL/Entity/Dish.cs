e35ca71677a2e0ffbf562b62b5cef4bbc9ce3127
LiSJ321 <126954436+LiSJ321@users.noreply.github.com>
2024-12-24 22:16:29 +0800

    feat(database):fixed db entity, add reqquired fileds

--- Delivery_DAL/Entity/Dish.cs
+++ Delivery_DAL/Entity/Dish.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Delivery_DAL.Enum;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
--- Delivery_DAL/Entity/Order.cs
+++ Delivery_DAL/Entity/Order.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Delivery_DAL.Enum;
 
 namespace Delivery_DAL.Entity
 {
-    internal class Order
+    public class Order
     {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public List<OrderBasket> OrderBaskets { get; set; }
+        [Required]
+        public DateTime DeliveryTime { get; set; }
+
+        [Required]
+        public DateTime OrderTime { get; set; }
+
+        [Required]
+        public OrderStatus Status { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Address { get; set; }
     }
 }
