a333e5cf0c7cab45d67d0d52a28ce097c2206ef9
LiSJ321 <126954436+LiSJ321@users.noreply.github.com>
2024-12-24 22:12:08 +0800

    feat(database): created all entities

---
+++ Delivery_DAL/Entity/Dish.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delivery_DAL.Entity
+{
+    public class Dish
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        public string? Image { get; set; }
+
+        public bool Vegetarian { get; set; }
+
+        public double Rating { get; set; }
+
+        public DishCategory Category { get; set; }
+    }
+}
