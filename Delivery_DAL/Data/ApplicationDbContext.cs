05f6dd4c4de94f1a6923adee7b6f77b4cf3d4a29
LiSJ321 <126954436+LiSJ321@users.noreply.github.com>
2024-12-24 22:21:00 +0800

    feat: add migration

--- Delivery_DAL/Data/ApplicationDbContext.cs
+++ Delivery_DAL/Data/ApplicationDbContext.cs
@@ -8,5 +8,16 @@ namespace Delivery_DAL.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Dish> Dishes { get; set; }
+
+        public DbSet<Basket> Baskets { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderBasket> OrderBaskets { get; set; }
+
+        public DbSet<Rating> Ratings { get; set; }
     }
 }
