0826120ec128ab5dc57dcca36705bfc33b175a85
LiSJ321 <126954436+LiSJ321@users.noreply.github.com>
2024-12-27 21:38:29 +0800

    feat(basket): Implement basket management methods (Get, Add, Delete)
    - Added GetBasket method to retrieve a list of dishes in a user's basket
    - Implemented AddBasket method to add a dish to the user's basket, creating a new entry if the dish is not already in the basket, or incrementing the amount if it exists.
    - Implemented DeleteBasket method to remove a dish from the basket, either decreasing the amount or completely removing the item if the amount reaches 1 or the 'increase' flag is false.
    - Included error handling for cases where a dish is not found in the database or basket.

---
+++ Delivery_DAL/Dto/DishBasketDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delivery_DAL.Dto
+{
+    public class DishBasketDto
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public double TotalPrice { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        public string? Image { get; set; }
+    }
+}
