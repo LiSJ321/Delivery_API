5fceb19f239846df37c1595b356bc014b6f64ede
LiSJ321 <126954436+LiSJ321@users.noreply.github.com>
2024-12-27 21:50:31 +0800

    Fixed jwt,swagger configurations

--- Delivery_API/Program.cs
+++ Delivery_API/Program.cs
@@ -5,8 +5,59 @@ using Delivery_DAL.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Delivery_API",
+        Version = "v1",
+        Description = ""
+    });
+
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Description = "Please enter token",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer",
+        Type = SecuritySchemeType.Http,
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+
+    options.AddSecurityDefinition("Bearer", securityScheme);
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { securityScheme, new List<string>() }
+    });
+});
+
+
+builder.Services.Configure<JwtConfig>(
+    builder.Configuration.GetSection("Jwt"));
+var jwtConfig = builder.Configuration.GetSection("Jwt").Get<JwtConfig>();
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidIssuer = jwtConfig.Issuer,
+            ValidAudience = jwtConfig.Audience,
+            ValidateIssuer = true,
+            ValidateLifetime = jwtConfig.ValidateLifetime,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.SigningKey))
+        };
+    });
 
 var app = builder.Build();
 
@@ -16,10 +67,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
 
 app.MapControllers();
 
